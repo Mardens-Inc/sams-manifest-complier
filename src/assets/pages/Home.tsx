1e95c4af3881572f1f420cb6be499ef3d538b595
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-06-30 10:16:29 -0400

    Update Home component to integrate category selection with manifest parsing

--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -1,30 +1,30 @@
 import DragDropArea from "../components/DragDropArea.tsx";
 import {useEffect, useState} from "react";
-import {getManifestFromFiles} from "../ts/manifest_parser.ts";
+import {getManifestFromFiles, Manifest} from "../ts/manifest_parser.ts";
+import SelectCategoriesToExport from "../components/SelectCategoriesToExport.tsx";
 
 export default function Home()
 {
     const [files, setFiles] = useState<string[]>([]);
+    const [manifest, setManifest] = useState<Manifest[]>([]);
     useEffect(() =>
     {
         if (files.length == 0) return;
-        getManifestFromFiles(files).then(console.log);
+        getManifestFromFiles(files).then(setManifest);
     }, [files]);
-    return (
-        <>
-            {files.length == 0 ?
-                <DragDropArea onFileSelected={setFiles}/>
-                :
-                <>
-                    <h1>Files Selected:</h1>
-                    <ul>
-                        {files.map((file, index) => (
-                            <li key={index}>{file}</li>
-                        ))}
-                    </ul>
-                    <button onClick={() => setFiles([])}>Clear Files</button>
-                </>
-            }
-        </>
-    );
+
+    if (files.length === 0)
+    {
+        return <DragDropArea onFileSelected={setFiles}/>;
+    } else if (manifest.length > 0)
+    {
+        return <SelectCategoriesToExport manifest={manifest} onSubmit={(categories) =>
+        {
+            // Handle the selected categories for export
+            console.log("Selected categories for export:", categories);
+        }}/>;
+
+    }
+
+    return null;
 }
